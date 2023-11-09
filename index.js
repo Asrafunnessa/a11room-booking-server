436ee93a6b79bd3127d92000473c44b3d246f9ea
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-09 09:22:30 +0600

    load rooms data

--- index.js
+++ index.js
@@ -108,7 +108,12 @@ async function run() {
       res.send(result);
     })
 
-    
+    app.get('/rooms/:id', async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: new ObjectId(id)}
+       const result = await roomCollection.findOne(query);
+       res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
